bc263fb6e72734b4b795fdbd9cc037de1cb793b8
George Nentidis <gnentid@msn.com>
2013-07-17 14:29:40 +0300

    web.js require fixed.

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fs = requre('fs');
+  fs = require('fs');
   var buffer = fs.readFileSync("index.html");
   response.send(buffer.toString());
   //response.send('Hello World 2!');
